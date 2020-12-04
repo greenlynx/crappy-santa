9d4e36e60e4aed4b665bfda4f927d41237424207
Dan Haughey <dan@greenlynx.co.uk>
2020-12-04 08:49:48 +0000

    WIP

--- generate.js
+++ generate.js
@@ -20,21 +20,19 @@ const snowflakeCount = 50
 let snowflakes = ''
 let snowflakeStyles = ''
 
+const lookup = {
+  0: 'A'
+}
+const encode = (val) => {
+
+}
+
 var rng = seedrandom(randomSeed)
 
 for (let i = 0; i < snowflakeCount; i++) {
   const distance = rng()
   snowflakes += '<p>❄️</p>'
-  snowflakeStyles += `.snow p:nth-child(${i + 1}) {
---distance: ${distance};
-z-index: ${((1-distance) * 100 - 50).toFixed(0)};
-font-size: ${(1-distance) * 45}px;
-left: ${rng() * 100}%;
-animation: snowfall ${distance * 20 + 4}s -${rng() * 10 + 2}s infinite linear, spin ${rng() * 2500 + 2500}ms 0s infinite linear, float 5s ${rng() * 5}s infinite linear;
-opacity: ${(1 - distance) * 0.5 + 0.5};
-filter: blur(${distance * 2}px);
-}
-`
+  snowflakeStyles += `.snow p:nth-child(${i + 1}) {--d: ${distance};--e: ${1 - distance};z-index: ${((1-distance) * 100 - 50).toFixed(0)};left: ${rng() * 100}%;animation: snowfall ${distance * 20 + 4}s -${rng() * 10 + 2}s infinite linear, spin ${rng() * 2500 + 2500}ms 0s infinite linear, float 5s ${rng() * 5}s infinite linear;}`
 }
 
 let letters = ''
@@ -72,7 +70,7 @@ require("get-pixels")(imageFile, function (err, pixels) {
       const yOut = y / sampleEveryYPixels
 
       const id = `_${(lastId++).toString(36)}`
-      const element = `<hr id="${id}">`
+      const element = `<p id="${id}">`
 
       let lastKeyframe = ''
       let bareKeyframesArray = []
@@ -102,10 +100,18 @@ require("get-pixels")(imageFile, function (err, pixels) {
         if (a === 0) emptyFrameCount++
 
         const pad = x => x.length > 1 ? x : `0${x}`
+        
+        const compress = (r, g, b, a) => {
+          if (a === 'ff') {
+            return r[0] === r[1] && g[0] === g[1] && b[0] === b[1] ? `${r[0]}${g[0]}${b[0]}` : `${r}${g}${b}`
+          } else {
+            return r[0] === r[1] && g[0] === g[1] && b[0] === b[1] && a[0] === a[1] ? `${r[0]}${g[0]}${b[0]}${a[0]}` : `${r}${g}${b}${a}`
+          }
+        }
 
-        const bareKeyframe = `{background:#${pad(r.toString(16))}${pad(g.toString(16))}${pad(b.toString(16))}${pad(a.toString(16))}}`
+        const bareKeyframe = `{background:#${compress(pad(r.toString(16)), pad(g.toString(16)), pad(b.toString(16)), pad(a.toString(16)))}}`
         const keyframe = `${percentage}%${bareKeyframe}`
-        if (!lastKeyframe) lastKeyframe = `100%${bareKeyframe}`
+        if (!lastKeyframe) lastKeyframe = `end${bareKeyframe}`
 
         bareKeyframesArray.push(bareKeyframe)
         if (frame === 0 || bareKeyframesArray[frame - 1] !== bareKeyframe) {
@@ -114,7 +120,7 @@ require("get-pixels")(imageFile, function (err, pixels) {
       }
     
       if (emptyFrameCount !== frameCount) {
-        keyframesArray.push(lastKeyframe)
+        //keyframesArray.push(lastKeyframe)
 
         const keyframes = keyframesArray.join('')
 
@@ -135,117 +141,88 @@ require("get-pixels")(imageFile, function (err, pixels) {
 }
 
   const template = `<html>
-<head><meta charset=“UTF-8”></head>
-<body>
-<style>
-  :root {
-    --width: ${sampleEveryXPixels * scaleX}px;
-    --height: ${sampleEveryYPixels * scaleY}px;
-  }
-  div,hr {
-    display: block;
-    position: absolute;
-    padding: 0;
-    margin: 0;
-    border: 0;
-  }
-  #bg {
-    top: 0;
-    left: 0;
-    z-index: -100;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(0deg, rgba(10,10,115,1) 0%, rgba(2,0,36,1) 100%);
-  }
-  @keyframes fadein {
-    0% { opacity: 0; }
-    80% { opacity: 0; }
-    100% { opacity: 1; }
-  }
-  .santa {
-    opacity: 1;
-    animation-iteration-count: 1;
-    animation-name: fade-in;
-    animation-duration: 5s;
-    z-index: 0;
-    animation: fly 30s infinite linear;
-  }
-  .santa hr {
-    width: ${sampleEveryXPixels * scaleX}px;
-    height: ${sampleEveryXPixels * scaleX}px;
-    left: calc(var(--width) * var(--x));
-    top: calc(var(--height) * var(--y));
-    animation: var(--a) ${animationDuration}s infinite steps(1, end);
-  }
-  .roof {
-    width: 0; 
-    height: 0;
-    bottom: 0;
-    border-left: calc(var(--s) * 45) solid transparent;
-    border-right: calc(var(--s) * 45) solid transparent;
-    
-    border-bottom: calc(var(--s) * 45) solid red;
-  }
-
-  @keyframes fly {
-    from { left: -${width * 1}px; top: 40%; }
-    to { left: 100%; top: 20%; }
-  }
-  @keyframes spin {
-    from { transform:rotate(0deg); }
-    to { transform:rotate(360deg); }
-  }
-  @keyframes spin {
-    from {
-        transform:rotate(0deg);
-    }
-    to {
-        transform:rotate(360deg);
-    }
-  }
-  @keyframes snowfall {
-    0% { top: -5%; }
-    100% { top: 100%; }
-  }
-  @keyframes snowdrift {
-    0% { left: 100%; }
-    100% { left: -5%; }
-  }
-  @keyframes float {
-    from, to {
-      transform: translateX(calc((1 - var(--distance)) * -20px));
-    }
-    50% {
-      transform: translateX(calc((1 - var(--distance)) * 20px));
-    }
-  }
-  .snow {
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-  }
-  .snow p {
-    display: block;
-    position: absolute;
-    margin: 0;
-    padding: 0;
-  }
-  .moon {
-    border-radius: 50%;
-    box-shadow: 25px 10px 0px 0px #d4d27b;
-    right: 5%;
-    top: 5%;
-    width: 115px;
-    height: 115px;
-    z-index: -99;
-    filter: blur(4px);
-    transform: rotate(-30deg);
-  }
-  ${lettersStyles}
-  ${pixelStyles}
-  ${snowflakeStyles}
-  ${allKeyframes}
+<head><meta charset=“UTF-8”></head><body><style>
+:root {
+--width: ${sampleEveryXPixels * scaleX}px;
+--height: ${sampleEveryYPixels * scaleY}px;
+}
+div,p {
+display: block;
+position: absolute;
+padding: 0;
+margin: 0;
+border: 0;
+}
+#bg {
+top: 0;
+left: 0;
+z-index: -100;
+width: 100%;
+height: 100%;
+background: linear-gradient(0deg, rgba(10,10,115,1) 0%, rgba(2,0,36,1) 100%);
+}
+@keyframes fadein {
+0% { opacity: 0; }
+80% { opacity: 0; }
+100% { opacity: 1; }
+}
+.santa {
+opacity: 1;
+animation: fly 30s infinite linear;
+}
+.santa p {
+width: ${sampleEveryXPixels * scaleX}px;
+height: ${sampleEveryXPixels * scaleX}px;
+left: calc(var(--width) * var(--x));
+top: calc(var(--height) * var(--y));
+animation: var(--a) ${animationDuration}s infinite steps(1, end);
+}
+@keyframes fly {
+from { left: -${width * 1}px; top: 40%; }
+to { left: 100%; top: 20%; }
+}
+@keyframes spin {
+from { transform:rotate(0deg); }
+to { transform:rotate(360deg); }
+}
+@keyframes snowfall {
+0% { top: -5%; }
+100% { top: 100%; }
+}
+@keyframes float {
+from, to {
+transform: translateX(calc((1 - var(--d)) * -20px));
+}
+50% {
+transform: translateX(calc((1 - var(--d)) * 20px));
+}
+}
+.snow {
+left: 0px;
+top: 0px;
+width: 100%;
+height: 100%;
+}
+.snow p {
+display: block;
+position: absolute;
+margin: 0;
+padding: 0;
+filter: blur(calc(var(--d) * 2px));
+opacity: calc((1 - var(--d)) * 0.5 + 0.5);
+font-size: calc((var(--e)) * 45px);
+}
+.moon {
+border-radius: 50%;
+box-shadow: 25px 10px 0px 0px #d4d27b;
+right: 5%;
+top: 5%;
+width: 115px;
+height: 115px;
+z-index: -99;
+filter: blur(4px);
+transform: rotate(-30deg);
+}${pixelStyles}${snowflakeStyles}${allKeyframes}
 </style>
 <div id="bg">
 <div class="santa">
@@ -254,10 +231,7 @@ ${elements}
 <div class="snow">
 ${snowflakes}
 </div>
-<div class="moon"></div>
-</body>
-  </html>`
+<div class="moon"></div></body></html>`
 
   console.log(template)
 })
-
